944073ce1587c91af1c67782497d4041fb8877c9
Ahmet Enes Bayraktar <bayraktarahmetenes@gmail.com>
2022-03-17 06:13:41 +0300

    add ffi package

--- bin/steamworks_gen.dart
+++ bin/steamworks_gen.dart
@@ -72,6 +72,8 @@ Future<void> main(List<String> arguments) async {
   await steamworks_gen.generate(argResults.rest.first, output, target);
 
   // double format call required because of 'require_trailing_commas'
+  await Process.run("dart", ["pub", "add", "dart pub add ffi:1.2.0-dev.0"]);
+  await Process.run("dart", ["pub", "get"]);
   await Process.run("dart", ["format", "."], workingDirectory: output);
   await Process.run("dart", ["fix", "--apply"], workingDirectory: output);
   await Process.run("dart", ["format", "."], workingDirectory: output);
